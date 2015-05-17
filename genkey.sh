7e8ba2333fb227fba5b20c10a2f7e4b25c0c9cb4
ayy lmao <pikachupacket@yahoo.ro>
2015-05-17 09:03:21 -0400

    generate random key with /dev/urandom

--- genkey.sh
+++ genkey.sh
@@ -9,4 +9,10 @@ if [ "$#" -ne 1 ]; then
    exit
 fi
 
-head -c $1 /dev/urandom | tr -cd "A-Za-z0-9@#\!\$%^&*()_+=-~;,.<>/[]{}|?:'\\\`" && echo
+entropy=$(cat /proc/sys/kernel/random/entropy_avail)
+
+if [ $entropy -lt $1 ]; then
+   echo "Entropy is lower than bytesize..."
+else
+   head -c $1 /dev/urandom | tr -cd "A-Za-z0-9@#\!\$%^&*()_+=-~;,.<>/[]{}|?:'\\\`" && echo
+fi
